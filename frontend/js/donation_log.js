f3b0d2886217703b434504847ccc77ce4c084448
DoDOhyeon <jd105@naver.com>
2023-11-01 17:48:32 +0900

    test

--- frontend/js/donation_log.js
+++ frontend/js/donation_log.js
@@ -53,3 +53,15 @@ document.addEventListener("DOMContentLoaded", function () {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 });
+
+function toggleBtn1() {
+  
+  // 토글 할 버튼 선택 (btn1)
+  const btn1 = document.getElementById('btn1');
+  
+  // btn1 숨기기 (display: none)
+  if(btn1.style.display !== 'none') {
+    btn1.style.display = 'none';
+  }
+  
+}
